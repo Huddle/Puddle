596fba1209093ba5f69b82d51a20969e8d487b16
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2012-04-04 10:00:36 +0100

    bug fixes

---
+++ src/Folder.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Puddle
+{
+    public class Folder
+    {
+        public Folder(string description, string title, DateTime createdDt, DateTime updatedDt)
+        {
+            Description = description;
+            Title = title;
+        }
+
+        public string Description { get; private set; }
+        public string Title { get; private set; }
+        public DateTime Created { get; private set; }
+        public DateTime Updated { get; private set; }
+    }
+}
---
+++ src/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Puddle
+{
+    public class User
+    {
+        private readonly string _topLevelDomain;
+        private readonly string _authorizationClientId;
+
+        public User(int id)
+        {
+            _topLevelDomain = "local";
+            _authorizationClientId = "integration-test";
+            Id = id;
+            Token = "{'iss':'login.huddle." + _topLevelDomain + "', 'exp':1986681600, 'urn:huddle.claims.userid':" + Id + ", 'urn:huddle.claims.clientId':'" + _authorizationClientId + "'}";
+        }
+
+        protected int Id { get; private set; }
+        public string Token { get; private set; }
+    }
+}
